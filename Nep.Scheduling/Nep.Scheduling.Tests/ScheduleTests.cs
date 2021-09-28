1dde42f63a6545bc47b7c57aabdf016f7d06262e
Korporal <hugh_gleaves@hotmail.com>
2021-09-28 07:20:36 -0700

    Make one of the test intentionally fail.

--- Nep.Scheduling/Nep.Scheduling.Tests/ScheduleTests.cs
+++ Nep.Scheduling/Nep.Scheduling.Tests/ScheduleTests.cs
@@ -63,7 +63,7 @@ namespace Nep.Scheduling
 
             var s = schedule.Adjust(holiday, Adjustment.Apply, out _, 7);
 
-            Assert.IsTrue(s.Items[0].At == DT2021_JAN_01);
+            Assert.IsFalse(s.Items[0].At == DT2021_JAN_01);
             Assert.IsTrue(s.Items[1].At == DT2021_JAN_15);
             Assert.IsTrue(s.Items[2].At == DT2021_JAN_22);
         }
